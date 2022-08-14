8f7ba1f73424fda3f6fc4f47e91a8d361f5f3d98
Tyler Harker <me@tylerharker.com>
2022-08-14 07:54:32 -0700

    chore: updated entities

--- DungeonOT.Data/Entities/GenderEntity.cs
+++ DungeonOT.Data/Entities/GenderEntity.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DungeonOT.Data.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,4 +7,11 @@ using System.Threading.Tasks;
 
 namespace DungeonOT.Data.Entities
 {
+    public class GenderEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Gender Value { get; set; }
+    }
+    
 }
--- DungeonOT.Data/Entities/PlayerEntity.cs
+++ DungeonOT.Data/Entities/PlayerEntity.cs
@@ -1,6 +1,8 @@
 ﻿using DungeonOT.Data.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +11,11 @@ namespace DungeonOT.Data.Entities
 {
     public class PlayerEntity
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public int AccountId { get;set; }
         public string Name { get; set; }
-        public Gender Gender { get; set; }
+        public GenderEntity Gender { get; set; }
     }
 }
